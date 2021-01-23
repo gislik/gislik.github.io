9172da12cd70321ac4842a396dc05253f26a1709
Gísli Kristjánsson <gislik@hamstur.is>
2021-01-23 02:39:18 +0000

    17167

--- publish.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Adding new content and removing old"
-git -C gislik.github.io add --all .
-echo "Comitting changes"
-git -C gislik.github.io commit -a -m "$$"
-echo "Pushing repository to GitHub"
-git -C gislik.github.io push origin master
--- watch.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-stack run blog watch
